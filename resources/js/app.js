505effd12b8e7210c18c50244a178a22b888de72
vlada <vladapantic@hotmail.com>
2021-07-02 12:44:29 +0200

    Install Laravel

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue';
+require('./bootstrap');
+
+let app = createApp({});
+
+app.component('example-component',require('./components/ExampleComponent.vue').default);
+
+app.mount('#app');
